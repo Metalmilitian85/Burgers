f1c3951548962999e8b760195d4bc92172360130
Tim <timward1985@gmail.com>
2023-08-16 15:31:25 -0500

    commentbox

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -10,7 +10,7 @@ export default function Form() {
       <h2 className="mb-5 mt-10 text-2xl">Leave us a Review!</h2>
       <p className="text-center mb-5 font-semibold text-lg mx-7">We value your input! Please leave a comment if there are any questions, 
       concerns, or anything you'd like us to know!:</p>
-      <textarea className="border-2 m-auto max-w-[1240px]"
+      <textarea className="border-2 m-auto w-[70%]"
         name="postContent"
         defaultValue="Comment here"
         rows={4}
